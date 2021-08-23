53afdea361f4d3cdb815e74c86e87406a19dcfed
satishchhatpar <satish.chhatpar@gmail.com>
2021-08-23 12:49:57 +0530

    Initial commit

---
+++ src/main/java/com/nextsaa/hms/HmsApplication.java
@@ -0,0 +1,14 @@
+package com.nextsaa.hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmsApplication.class, args);
+	}
+
+}
