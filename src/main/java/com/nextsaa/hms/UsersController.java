cf11bd3482e747ee51496cf13bc3a0e558efa0ad
Arjun Dhawale <dhawalearjun@gmail.com>
2021-08-24 21:47:58 +0530

    login issue

--- src/main/java/com/nextsaa/hms/UsersController.java
+++ src/main/java/com/nextsaa/hms/UsersController.java
@@ -1,10 +1,21 @@
 package com.nextsaa.hms;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UsersController {
+	
+	@GetMapping("/")
+    public String getForwardToCorrectPage() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null && auth.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"))) {
+		   return "adminPage";
+		}
+		return "userPage";
+    }
 
     @GetMapping("/protectedLinks")
     public String getAnonymousPage() {
---
+++ src/main/java/com/nextsaa/hms/util/Utility.java
@@ -0,0 +1,18 @@
+package com.nextsaa.hms.util;
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class Utility {
+	
+	public static String getUserName(){
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (!(authentication instanceof AnonymousAuthenticationToken)) {
+		    String currentUserName = authentication.getName();
+		    return currentUserName;
+		}
+		return "";
+	}
+
+}
